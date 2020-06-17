4abce95461e73a9b28238a7813e980bb9e938e7d
marcosnasa <marcosgnasa@gmail.com>
2020-06-17 17:07:42 +0200

    Scopes recognized and ready to be drawn.

--- src/app/main.js
+++ src/app/main.js
@@ -1,14 +1,25 @@
+/* eslint-disable guard-for-in */
 'use strict';
 
 var app = (function App() {
-  const defaultScope = {
-    name: 'global',
-    varDeclarations: [],
-    varAccesses: [],
-    scopes: [],
-    color: '#db1d0f',
-  };
+  // const defaultScope = {
+  //   name: 'global',
+  //   varDeclarations: [],
+  //   varAccesses: [],
+  //   scopes: [],
+  //   startLine: null,
+  //   endLine: null,
+  //   color: '#db1d0f',
+  // };
   const squareFallback = { x: 0, y: 0, width: 0, height: 0 };
+  const defaultCode = {
+    value: '',
+    rawValue: '',
+    codeLines: [],
+    parsed: undefined,
+    scopes: [],
+    valid: true,
+  };
   var code;
   var codeEditor;
   var codeDisplayer;
@@ -21,27 +32,24 @@ var app = (function App() {
    * Initialize the app.
    */
   function bootstrap() {
-    code = {
-      value: '',
-      parsed: undefined,
-      scopes: [{ ...defaultScope }],
-      scopesNumber: 1,
-      valid: true,
-    };
+    code = { ...defaultCode };
     codeEditor = setupCodeEditor(document.getElementById('code-editor'), false);
     codeEditor.save();
-    codeDisplayer = setupCodeEditor(document.getElementById('code-displayer'), true);
+    codeDisplayer = setupCodeEditor(
+      document.getElementById('code-displayer'),
+      true,
+    );
     scopeBubbles = document.getElementById('scopes');
-    // visualizer = document.querySelector(
-    //   '.code-displayer > .CodeMirror .CodeMirror-code',
-    // );
+    visualizer = document.querySelector(
+      '.code-displayer > .CodeMirror .CodeMirror-code',
+    );
 
     codeEditor.on('change', onInput);
+    window.addEventListener('resize', onResize);
     return codeEditor;
   }
 
   function setupCodeEditor(codeEditor, readOnly) {
-    CodeMirror.colorize();
     var codeMirror = CodeMirror.fromTextArea(codeEditor, {
       mode: 'application/javascript',
       value: ``,
@@ -67,8 +75,17 @@ var app = (function App() {
   /**
    *
    */
+  function onResize(event) {
+    updateCodeDisplayer(code);
+  }
+
+  /**
+   *
+   */
   function processCode(code, value) {
     var processedCode = JSON.parse(JSON.stringify(code));
+    processedCode.rawValue = value;
+
     try {
       processedCode.value = prettier
         .format(value, {
@@ -86,22 +103,81 @@ var app = (function App() {
       processedCode.valid = false;
     }
 
-    processedCode.parsed = processedCode.valid
-      ? esprima.parse(processedCode.value)
-      : null;
+    processedCode = processedCode.valid
+      ? {
+          ...processedCode,
+          parsed: esprima.parse(processedCode.value, {
+            range: true,
+            loc: true,
+            tokens: true,
+          }),
+        }
+      : processedCode;
 
-    processedCode = processScopes(Object.freeze(processedCode));
+    processedCode = processedCode.valid
+      ? processLines(Object.freeze(processedCode))
+      : processedCode;
+
+    processedCode = processedCode.valid
+      ? processScopes(Object.freeze(processedCode))
+      : processedCode;
 
     return processedCode;
+  }
+
+  function processLines(code) {
+    var codeProcessedLines = { ...code };
+    {
+      let previousChars = 0;
+      var codeLines = codeProcessedLines.value
+        .split(/(\n)/)
+        .map(function processLine(line) {
+          var processedLine = {
+            line,
+            startChar: previousChars,
+            endChar: previousChars + line.length,
+          };
+          previousChars = processedLine.endChar;
+          return processedLine;
+        })
+        .filter(function filterEmptyLines(lineObj) {
+          return lineObj.line !== '\n';
+        });
+    }
+
+    return { ...codeProcessedLines, codeLines };
   }
 
   /**
    *
    */
   function processScopes(code) {
-    var processedScopes = [{ ...defaultScope }];
+    var scopes = escope.analyze(Object.freeze(code.parsed), { ecmaVersion: 6 });
 
-    return { ...code, scope: processedScopes };
+    var levels = eslevels.levels(Object.freeze(code.parsed), {
+      mode: 'full',
+      escopeOpts: {
+        ecmaVersion: 6,
+      },
+    });
+
+    var levelsNumber = [
+      ...new Set(
+        [...levels].map((level) => {
+          return level[0];
+        }),
+      ),
+    ].length;
+
+    return {
+      ...code,
+      scopes: {
+        ...scopes,
+        scopesNumber: scopes.scopes.length,
+        levels,
+        levelsNumber,
+      },
+    };
   }
 
   /**
@@ -109,19 +185,18 @@ var app = (function App() {
    */
   function updateCodeDisplayer(code) {
     requestAnimationFrame(() => {
-      // write(code.value);
       codeDisplayer.setValue(code.value);
     });
+
+    requestAnimationFrame(function clearBubbles() {
+      restart();
+      clearVisualization();
+    });
+
     if (code.valid && code.value) {
-      requestAnimationFrame(function clearBubbles() {
-        clearVisualization(code);
-      });
-      requestAnimationFrame(function drawBubbles() {
-        setVisualization(code);
-      });
-    } else {
-      requestAnimationFrame(function clearBubbles() {
-        clearVisualization(code);
+      requestAnimationFrame(function setVisualization() {
+        code = setBubbles(code);
+        drawBubbles(code);
       });
     }
   }
@@ -129,41 +204,70 @@ var app = (function App() {
   /**
    *
    */
-  function write(code) {
-    codeDisplayer.innerHTML = '';
-    var codeLines = code.split('\n');
-    codeLines.forEach((codeLine) => {
-      var line = document.createElement('div');
-      {
-        let codeFragments = codeLine.split('\t');
-        codeFragments.forEach((codeFragment) => {
-          if (codeFragment == '') {
-            var tab = document.createElement('span');
-            tab.classList.add('tab');
-            line.appendChild(tab);
-          }
-        });
+  function setBubbles({ codeLines, scopes, ...rest }) {
+    var visualizerFigure = visualizer.getBoundingClientRect();
+    drawBubble(visualizerFigure, visualizerFigure);
+
+    var codeLineElements = [
+      ...visualizer.getElementsByClassName('CodeMirror-line'),
+    ];
+
+    var bubbles = {
+      scopes: [],
+    };
+    for (let scope of scopes.scopes) {
+      let {
+        block: {
+          range: [scopeRangeStart, scopeRangeEnd],
+        },
+        variables,
+      } = scope;
+      // let [scopeRangeStart, scopeRangeEnd] = scope.block.range;
+      let scopeBubbleLines = [];
+      let scopeBubbleVariables = [];
+      for (let i = 0; i < codeLines.length; i++) {
+        if (
+          codeLines[i].startChar >= scopeRangeStart &&
+          codeLines[i].endChar <= scopeRangeEnd
+        ) {
+          scopeBubbleLines.push(codeLineElements[i]);
+          // scopeBubbleVariables.push(codeLineElements[i]);
+        }
       }
-      {
-        let lineCode = document.createElement('span');
-        lineCode.textContent = codeLine;
-        line.appendChild(lineCode);
-      }
-      line.classList.add('line');
-      codeDisplayer.appendChild(line);
-    });
+      bubbles.scopes.push({
+        lines: scopeBubbleLines,
+        variables,
+        color: randomColor(),
+      });
+    }
+
+    console.log(bubbles);
+
+    // for (let i = 0; i < codeLineElements.length; i++) {
+    //   if (codeLineElements[i].textContent.trim() == codeLines[i].line.trim()) {
+    //     console.log('equal');
+    //   } else if (
+    //     codeLineElements[i].textContent.trim() == '&#8203;' &&
+    //     codeLines[i].line.trim() == ''
+    //   ) {
+    //     console.log('equal empty');
+    //   } else {
+    //     console.log('not equal');
+    //     console.log(codeLineElements[i].textContent.trim());
+    //     console.log(codeLines[i].line.trim());
+    //   }
+    //   console.log(codeLineElements[i]);
+    //   console.log(codeLines[i]);
+    // }
+
+    console.log(scopes);
+    return { codeLines, scopes, ...rest };
   }
 
   /**
    *
    */
-  function setVisualization(code) {
-    // var visualizerFigure = visualizer.getBoundingClientRect();
-    // drawBubble(visualizerFigure, visualizerFigure);
-    // elements.forEach((e) => {
-    //   console.log(e.textContent);
-    // });
-  }
+  function drawBubbles({ codeLines, scopes, ...rest }) {}
 
   /**
    *
@@ -176,7 +280,7 @@ var app = (function App() {
       height: bubbleHeight = 0,
     } = squareFallback,
     { x = 0, y = 0 } = squareFallback,
-    color = defaultScope.color,
+    color = randomColor(),
   ) {
     var bubble = document.createElement('div');
     bubble.classList.add('scopes__bubble');
@@ -195,8 +299,15 @@ var app = (function App() {
   /**
    *
    */
-  function clearVisualization(code) {
+  function clearVisualization() {
     scopeBubbles.innerHTML = '';
+  }
+
+  /**
+   *
+   */
+  function restart() {
+    code = { ...defaultCode };
   }
 })();
 
