2ee9a0bafa2f7a2843a59c3a6c3f05e6df0c71f9
marcosnasa <marcosgnasa@gmail.com>
2020-07-22 16:13:06 +0200

    SW updated.

--- service-worker.js
+++ service-worker.js
@@ -28,6 +28,7 @@ const urlsToCache = [
   './assets/icons/icon-1080.png',
   './assets/icons/icon-maskable.png',
   './manifest.json',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap',
 ];
 
 self.addEventListener('install', onInstall);
@@ -88,63 +89,26 @@ function onFetch(event) {
     return;
   }
 
-  event.respondWith(handleFetch(request));
-}
+  event.respondWith(
+    (async function staleWhileRevalidate() {
+      const cache = await caches.open(CACHE_NAME);
 
-async function handleFetch(request) {
-  var url = new URL(request.url);
-  var reqURL = url.pathname;
-  var cache = await caches.open(CACHE_NAME);
+      const cachedResponsePromise = await cache.match(request);
+      const networkResponsePromise = fetch(request);
 
-  if (url.origin == location.origin) {
-    // request for site's own URL? => network-and-cache
-    let fetchOptions = {
-      method: request.method,
-      headers: request.headers,
-      cache: 'no-store',
-    };
+      if (request.url.startsWith(self.location.origin)) {
+        event.waitUntil(
+          (async function fetchNetwork() {
+            const networkResponse = await networkResponsePromise;
 
-    let res = await safeRequest(
-      reqURL,
-      request,
-      fetchOptions,
-      /* cacheResponse= */ false,
-      /* checkCacheFirst= */ false,
-      /* checkCacheLast= */ true,
-    );
-    if (res) {
-      if (!res.headers.get('X-Not-Found')) {
-        await cache.put(reqURL, res.clone());
-      } else {
-        await cache.delete(reqURL);
+            await cache.put(request, networkResponse.clone());
+          })(),
+        );
       }
-      return res;
-    }
 
-    // otherwise, return a 404 page
-    return cache.match('/404.html');
-  } else {
-    // all other files use "cache-first"
-
-    let fetchOptions = {
-      method: request.method,
-      headers: request.headers,
-      cache: 'no-store',
-    };
-    let res = await safeRequest(
-      reqURL,
-      request,
-      fetchOptions,
-      /* cacheResponse= */ true,
-      /* checkCacheFirst= */ true,
-    );
-    if (res) {
-      return res;
-    }
-
-    // otherwise, force a network-level 404 response
-    return notFoundResponse();
-  }
+      return cachedResponsePromise || networkResponsePromise;
+    })(),
+  );
 }
 
 async function handleActivation() {
@@ -166,53 +130,4 @@ async function clearCaches() {
       return caches.delete(cacheName);
     }),
   );
-}
-
-async function safeRequest(
-  reqURL,
-  request,
-  options,
-  cacheResponse = false,
-  checkCacheFirst = false,
-  checkCacheLast = false,
-  useRequestDirectly = false,
-) {
-  var cache = await caches.open(CACHE_NAME);
-  var res;
-
-  if (checkCacheFirst) {
-    res = await cache.match(reqURL);
-    if (res) {
-      return res;
-    }
-  }
-
-  try {
-    if (useRequestDirectly) {
-      res = await fetch(request, options);
-    } else {
-      res = await fetch(request.url, options);
-    }
-
-    if (res && (res.ok || res.type == 'opaqueredirect')) {
-      if (cacheResponse) {
-        await cache.put(reqURL, res.clone());
-      }
-      return res;
-    }
-  } catch (err) {}
-
-  if (checkCacheLast) {
-    res = await cache.match(reqURL);
-    if (res) {
-      return res;
-    }
-  }
-}
-
-function notFoundResponse() {
-  return new Response('', {
-    status: 404,
-    statusText: 'Not Found',
-  });
 }
